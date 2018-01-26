39f1f7aeddf70ff4ee1bcfbf42444a7b9120f863
jackson <jackson@swordfish.co.za>
2018-01-26 06:38:04 +0200

    get cam information from database
    send cam info to index.html
    
    send detail cam info to video stream

--- application.py
+++ application.py
@@ -1,10 +1,37 @@
-from flask import Flask
+import os
+
+import psycopg2
+from builtins import str
+from flask import Flask, render_template, request
+from psycopg2.extras import RealDictCursor
+
+DATABASE_NAME = 'cam_config'
+TABLE_NAME = 'config'
+PASSWORD = 'password'
+USER = os.environ['USER']
+CHANNEL = 'events'
+conn = psycopg2.connect(user=USER, database=DATABASE_NAME, password=PASSWORD)
+cur = conn.cursor(cursor_factory=RealDictCursor)
+
 application = Flask(__name__)
 
 
 @application.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+def index():
+    """Video streaming home page."""
+    try:
+        cur.execute('select * from port_map;')
+        # Fetch a dictionary with the database
+        port_map = cur.fetchall()
+    except Exception as ex:
+        print(ex)
+    return render_template('index.html', results=port_map)
+
+
+@application.route("/video_stream", methods=['GET','POST'])
+def video_stream():
+    """Video streaming detail"""
+    return render_template('video_stream.html', result=request.args)
 
 
 
